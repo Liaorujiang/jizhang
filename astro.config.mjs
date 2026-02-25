b5a4db297f50646b5f46b81a05c0c45edb215042
Liaorujiang <465241367@qq.com>
2026-02-25 21:05:42 +0800

    修改部署配置，使用 @astrojs/vercel 适配器，确保在 Vercel Pages 上正常运行

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  output: 'hybrid',
+  adapter: vercel({
+    edge: true
   })
 });
